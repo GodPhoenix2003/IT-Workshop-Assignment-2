b19db7a7da0c5119afe581fc10d5f1491a47f03b
Sagnik Chatterjee <100948753+GodPhoenix2003@users.noreply.github.com>
2022-08-30 01:51:12 +0530

    Add files via upload

---
+++ Prog11.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Prog11 {
+    public static void main(String []args){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the number to be reversed: ");
+        int x = in.nextInt();
+        int rev = 0, dup = x;
+        while(dup > 0){
+            rev = (rev * 10) + (dup % 10);
+            dup /= 10;
+        }
+        if(x == rev)
+            System.out.println(x + " is a Palindrome Number.");
+        else
+            System.out.println(x + " is not a Palindrome Number.");
+        in.close();
+    }
+}
