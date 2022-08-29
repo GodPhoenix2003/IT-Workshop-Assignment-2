4c0c380c4a26d97b7cfd8064fecb2b231443f151
Sagnik Chatterjee <100948753+GodPhoenix2003@users.noreply.github.com>
2022-08-30 02:00:41 +0530

    Update Prog11.java

--- Prog11.java
+++ Prog11.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class Prog11 {
     public static void main(String []args){
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter the number to be reversed: ");
+        System.out.print("Enter the number to be checked: ");
         int x = in.nextInt();
         int rev = 0, dup = x;
         while(dup > 0){
